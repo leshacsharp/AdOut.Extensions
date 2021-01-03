f5e1a6b92b14678a4eff80e06fae8f4cc8a8abfc
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2021-01-03 21:05:11 +0300

    Added a ResourceId attribute for resource based authoriaztion

---
+++ src/AdOut.Extensions/Authorization/ResourceIdAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AdOut.Extensions.Authorization
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter)]
+    public class ResourceIdAttribute : Attribute
+    {
+    }
+}
