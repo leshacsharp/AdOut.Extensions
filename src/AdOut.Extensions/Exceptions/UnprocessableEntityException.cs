5ca062dd1a90e15b6d4849a522bf915e9ed93cea
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2021-01-04 19:03:36 +0300

    Added an UnprocessableEntity exception

---
+++ src/AdOut.Extensions/Exceptions/UnprocessableEntityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdOut.Extensions.Exceptions
+{
+    public class UnprocessableEntityException : Exception, IHttpException
+    {
+        public UnprocessableEntityException(string message = null) : base(message)
+        {
+        }
+
+        public int HttpStatusCode => (int)System.Net.HttpStatusCode.UnprocessableEntity;
+    }
+}
