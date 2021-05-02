db23f65f59eff58cdc6b73cb7c151b0afa043209
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2021-05-02 16:20:40 +0300

    Added Swagger filters for proper generating openapi files

---
+++ src/AdOut.Extensions/Filters/SwaggerContentTypeFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Linq;
+
+namespace AdOut.Extensions.Filters
+{
+    class SwaggerContentTypeFilter : IOperationFilter
+    {
+        private readonly string[] _mediaTypes;
+        public SwaggerContentTypeFilter(string[] mediaTypes)
+        {
+            _mediaTypes = mediaTypes;
+        }
+
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.RequestBody != null)
+            {
+                foreach (var content in operation.RequestBody.Content)
+                {
+                    if (!_mediaTypes.Contains(content.Key))
+                    {
+                        operation.RequestBody.Content.Remove(content.Key);
+                    }
+                }
+            }
+
+            foreach (var response in operation.Responses)
+            {
+                foreach (var content in response.Value.Content)
+                {
+                    if (!_mediaTypes.Contains(content.Key))
+                    {
+                        response.Value.Content.Remove(content.Key);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/AdOut.Extensions/Filters/SwaggerDeleteProblemDetailsTypeFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AdOut.Extensions.Filters
+{
+    public class SwaggerDeleteProblemDetailsTypeFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            foreach (var response in operation.Responses)
+            {
+                foreach (var content in response.Value.Content)
+                {
+                    if (content.Value.Schema.Reference?.Id == nameof(ProblemDetails))
+                    {
+                        response.Value.Content.Remove(content.Key);
+                    }
+                }
+            }
+        }
+    }
+}
